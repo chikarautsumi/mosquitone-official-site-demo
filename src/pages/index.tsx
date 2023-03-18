58295247dcc0ba5e27f0bf35696ebcbc54195a4c
chikarautsumi <chiksrskobu0225@gmail.com>
2023-03-18 19:51:34 +0900

    theme-ui/mdxと@theme-ui/colorを実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import { alpha } from '@theme-ui/color';
+import { ThemeProvider, Button, Theme} from "theme-ui";
+import { Themed } from '@theme-ui/mdx'
+
+const theme: Theme = {
+  colors: {
+    text: '#fff',
+    primary: '#48f',
+    accent: 'f72',
+  },
+  space: [0, 4, 8, 12, 16, 20, 24, 32],
+  fontSizes: [12, 14, 16, 18, 20, 24, 32, 48, 64, 80],
+  breakpoints: ['480px', '768px', '1024px'],
+  buttons: {
+    primary: {
+      color: 'white',
+      backgroundColor: 'primary',
+      ':hover': {
+        backgroundColor: alpha('primary', 0.8),
+      }
+    },
+  },
+  styles: {
+    h1: {
+      fontSize: [5, 6, 7],
+      color: 'primary',
+      fontFamily: 'heading',
+      mt: [2, 3, 4],
+      mb: ['2px', 1, 2],
+    },
+  }
+};
 
 const pageStyles = {
   color: "#232129",
@@ -139,51 +171,56 @@ const links = [
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
+      <ThemeProvider theme={theme}>
+        <Themed.h1>テーマド</Themed.h1>
+        <h1 style={headingStyles}>
+          Congratulations
+          <br />
+          <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        </h1>
+        <p style={paragraphStyles}>
+          Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
+          update in real-time. 😎
+        </p>
+        <ul style={doclistStyles}>
+          {docLinks.map(doc => (
+            <li key={doc.url} style={docLinkStyle}>
               <a
                 style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
+                href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
               >
-                {link.text}
+                {doc.text}
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+            </li>
+          ))}
+        </ul>
+        <ul style={listStyles}>
+          {links.map(link => (
+            <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+              <span>
+                <a
+                  style={linkStyle}
+                  href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
+                >
+                  {link.text}
+                </a>
+                {link.badge && (
+                  <span style={badgeStyle} aria-label="New Badge">
+                    NEW!
+                  </span>
+                )}
+                <p style={descriptionStyle}>{link.description}</p>
+              </span>
+            </li>
+          ))}
+        </ul>
+        <img
+          alt="Gatsby G Logo"
+          src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+        />
+        
+        <Button variant='primary'>いい感じにホバーを設定できたー</Button>
+      </ThemeProvider>
     </main>
   )
 }
